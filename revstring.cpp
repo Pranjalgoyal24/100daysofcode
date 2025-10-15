4868ab569d85930f257ab1811c5342c3c15b6161
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2025-10-15 23:53:33 +0530

    Add files via upload

---
+++ revstring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    string str = "Pranjal Goyal";
+    reverse(str.begin(),str.end());
+    cout<<str;
+    return 0;
+
+}
