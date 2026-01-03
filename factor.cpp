e76c2680a29d640c4098cd9f9fd7c400fceb0830
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2026-01-03 22:43:12 +0530

    Add files via upload

---
+++ factor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Entr the number for factr:";
+    cin>>n;
+    int fact =1;
+    for(int i =1;i<=5;i++){
+        fact = fact*i;
+        
+      
+    }
+    cout<<"Hactors"<<fact;
+}
