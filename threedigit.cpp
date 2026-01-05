83934e09adeb2d4c044fa466d9f2a8767eadca3c
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2026-01-05 22:47:28 +0530

    Add files via upload

---
+++ threedigit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float n;
+    cout<<"Enter number : ";
+    cin>>n;
+    if(n>99 && n<1000 ){
+        cout<<"Yes it is a three digit number";
+    }
+    else
+    cout<<"Nota athree digitu";
+    return 0;
+}
