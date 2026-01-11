4531d44f35ae84868518fdb0ba1b51296786a673
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2026-01-11 21:10:00 +0530

    Add files via upload

---
+++ modulo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x =90;
+    int y =7;
+    cout << x%y;
+    // % ---> this called modulo operator which 
+    //provides remainder (e.g. 5%2 = quotient = 2 and reminder = 1;)
+
+
+    
+    return 0;
+
+}
