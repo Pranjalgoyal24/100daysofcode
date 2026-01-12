96a9771a37b4d440c09c1581135c4c51f015d6e6
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2026-01-12 21:48:00 +0530

    Add files via upload

---
+++ updation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x =5.9;
+    float y = 9.00004;
+    cout<<y<<endl;
+    cout<<x<<endl;
+
+    //cout<<x++ <<endl<< ++x;
+    x++;
+    cout<<x++;
+    cout<<endl<<x;
+}
