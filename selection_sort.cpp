e2faadb078a9d02712a69717456d6c35ddccad1a
Pranjal goyal <146417277+Pranjalgoyal24@users.noreply.github.com>
2026-03-01 23:46:48 +0530

    Add files via upload

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,27 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"ENter the size of array:";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter element of array:";
-    for(int i =0;i<n;i++){
-        cin>>arr[i];
+    int n=5;
+    // cout<<"Enter the size of array:";
+    // cin>>n;
+    int arr[n]={3,4,5,1,2};
+    cout<<"Enter elements of array: ";
+    // for(int i =0;i<n;i++){
+    //     cin>>arr[i];
+    // }
 
-    }
     for(int i =0;i<n-1;i++){
         int min_index=i;
         for(int j =i+1;j<n;j++){
-            if(arr[j]<arr[min_index]) {
-                            min_index=j;
-
+            if(arr[j]<arr[min_index]){
+                min_index=j;
             }
-           
         }
-         swap(arr[min_index],arr[i]);
-
+        swap(arr[min_index],arr[i]);
     }
+    
     for(int i =0;i<n;i++){
         cout<<arr[i]<<" ";
     }
